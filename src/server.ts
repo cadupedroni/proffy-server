0bb4994b9cdddcda340e2ca9d34b36af5608e119
Carlos Vieira <carlos.vieira@outlook.com>
2020-08-07 12:11:30 -0300

    Created Api Controllers  - classes  - connections Database Migrations  - create users  - create classes  - create class schedule  - create connections Utils  - convert hour to minutes Routes Server

---
+++ src/server.ts
@@ -0,0 +1,13 @@
+import express from 'express';
+import cors from 'cors';
+import routes from './routes';
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+app.use(routes);
+
+// Ouvir requisições http
+app.listen(3333);
+
